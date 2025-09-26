d9201634f3047854b9ec7dcac784902f032c3014
Matheus Emanuel <devmatheusefs@gmail.com>
2025-09-26 11:55:35 -0300

    Adiciona configuração inicial do Flask e modelos de banco de dados

--- sam/models.py
+++ sam/models.py
@@ -7,24 +7,24 @@ def load_usuario(id_usuario):
     return Usuario.queryy.get(int(id_usuario))
 
 class Usuario(database.Model, UserMixin):
-    id = database.Column(database.Intenger, primary_key = True) #diz que é a chave primaria
+    id = database.Column(database.Integer, primary_key = True) #diz que é a chave primaria
     nome = database.Column(database.String, nullable = False) # diz que o tipo número inteiro e que é um campo obrigatório
     email = database.Column(database.String, nullable = False, unique = True) # diz que o tipo é string, que é um campo obrigatório e que em todo o banco de dados a informação deve ser única(não deve haver repetidos)
     senha = database.Column(database.String, nullable = False)  # diz que o tipo é string e que é um campo obrigatório
 
 class Paciente(database.Model):
-    id = database.Column(database.Intenger, primary_key = True)
+    id = database.Column(database.Integer, primary_key = True)
     nome = database.Column(database.String, nullable = False)
 
 class Medicamento(database.Model):
-    id = database.Column(database.Intenger, primary_key = True)
+    id = database.Column(database.Integer, primary_key = True)
     nome = database.Column(database.String, nullable = False)
-    lote = database.Column(database.Intenger, nullable = False)
+    lote = database.Column(database.Integer, nullable = False)
     forma_adm = database.Column(database.String, nullable = False) 
 
 class Calculadora(database.Model):
-    id = id = database.Column(database.Intenger, primary_key = True)
-    qtd_prescrita = database.Column(database.Intenger, nullable = False)
+    id = id = database.Column(database.Integer, primary_key = True)
+    qtd_prescrita = database.Column(database.Integer, nullable = False)
     un_prescrita = database.Column(database.String, nullable = False)
-    qtd_requerida = database.Column(database.Intenger)
+    qtd_requerida = database.Column(database.Integer)
     un_requerida = database.Column(database.String, nullable = False)
--- sam/routes.py
+++ sam/routes.py
@@ -2,19 +2,24 @@ from flask import render_template, url_for, redirect
 from sam import app, database, bcrypt
 from sam.models import Usuario
 from flask_login import login_required, login_user, logout_user, current_user
-from sam.forms import FormLogin, FormCadastro, FormFoto
+from sam.forms import FormLogin, FormCadastro
 from werkzeug.utils import secure_filename
 import os
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
     form_login = FormLogin()
+    if form_login.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario)
+            return redirect(url_for("perfil", id_usuario=usuario.id))
+    return render_template("login.html", form=form_login)
 
 @app.route("/cadastro")
 def cadastro():
     form_cadastro =  FormCadastro()
 
-@app.route("/calculadora")
-def cadastro():
     
+
     
